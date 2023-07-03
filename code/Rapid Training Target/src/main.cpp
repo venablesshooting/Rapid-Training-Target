236e227ff541fa21f2a5eebfe9546fa92838fe83
TomBigSis <127209186+TomBigSis@users.noreply.github.com>
2023-07-03 12:59:25 +0100

    Added production files and logo start

--- code/Rapid Training Target/src/main.cpp
+++ code/Rapid Training Target/src/main.cpp
@@ -21,16 +21,16 @@ int Green = GPIO_NUM_5;
 // callback function that will be executed when data is received
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   memcpy(&myData, incomingData, sizeof(myData));
-  Serial.print("Bytes received: ");
-  Serial.println(len);
-  Serial.print("Series: ");
-  Serial.println(myData.b);
+  //Serial.print("Bytes received: ");
+  //Serial.println(len);
+  //Serial.print("Series: ");
+  //Serial.println(myData.b);
   runsequence();
 }
  
 void setup() {
 
-
+  pinMode(GPIO_NUM_8, OUTPUT);
   pinMode(Red, OUTPUT);
   pinMode(Green, OUTPUT);
   pinMode(Blue, OUTPUT);
@@ -47,9 +47,46 @@ void setup() {
   // Init ESP-NOW
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
+    digitalWrite(GPIO_NUM_8, HIGH);
     return;
   }
+  digitalWrite(GPIO_NUM_8, LOW);
+  Serial.println("No Error initializing ESP-NOW");
+  Serial.print("ESP Board BASE MAC Address:  ");
+  Serial.println(WiFi.macAddress());
   
+  uint8_t sta_mac[6];
+  char buffer [6];
+  esp_efuse_mac_get_default(sta_mac);
+
+  esp_read_mac(sta_mac, ESP_MAC_WIFI_SOFTAP);
+
+  Serial.print("SOFT AP MAC Address:  ");
+  
+  sprintf(buffer, "%02X:%02X:%02X:%02X:%02X:%02X", sta_mac[0], sta_mac[1], sta_mac[2], sta_mac[3], sta_mac[4], sta_mac[5], sta_mac[6]);
+  Serial.println(buffer);
+
+  esp_read_mac(sta_mac, ESP_MAC_WIFI_STA);
+
+  Serial.print("STATION MAC Address:  ");
+  
+  sprintf(buffer, "%02X:%02X:%02X:%02X:%02X:%02X", sta_mac[0], sta_mac[1], sta_mac[2], sta_mac[3], sta_mac[4], sta_mac[5], sta_mac[6]);
+  Serial.println(buffer);
+
+  esp_read_mac(sta_mac, ESP_MAC_BT);
+
+  Serial.print("BLUETOOTH MAC Address:  ");
+  
+  sprintf(buffer, "%02X:%02X:%02X:%02X:%02X:%02X", sta_mac[0], sta_mac[1], sta_mac[2], sta_mac[3], sta_mac[4], sta_mac[5], sta_mac[6]);
+  Serial.println(buffer);
+
+  esp_read_mac(sta_mac, ESP_MAC_ETH);
+
+  Serial.print("ETHERNET MAC Address:  ");
+  
+  sprintf(buffer, "%02X:%02X:%02X:%02X:%02X:%02X", sta_mac[0], sta_mac[1], sta_mac[2], sta_mac[3], sta_mac[4], sta_mac[5], sta_mac[6]);
+  Serial.println(buffer);
+
   // Once ESPNow is successfully Init, we will register for recv CB to
   // get recv packer info
   esp_now_register_recv_cb(OnDataRecv);
